8823ae8fddb96c011845730d40dd95b180879bb5
EricGeorge <eegee.dev@gmail.com>
2025-07-23 19:14:58 -0700

    Trivial implementation of  chapter 1 exercise

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,41 @@ use approx::AbsDiffEq;
 use std::ops;
 
 fn main() {
-    println!("Hello, world!");
+    exercise_1();
+}
+
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+struct Environment {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+fn exercise_1() {
+    let e = Environment {
+        gravity: vector(0.0, -0.1, 0.0),
+        wind: vector(-0.01, 0.0, 0.0),
+    };
+
+    let mut p = Projectile {
+        position: point(0.0, 1.0, 0.0),
+        velocity: (vector(1.0, 1.0, 0.0).normalize()) * 10.0,
+    };
+
+    let mut ticks = 0;
+
+    while p.position.y > 0.0 {
+        println!(
+            "Time: {:?}, x: {:?}, y: {:?}",
+            ticks, p.position.x, p.position.y
+        );
+        p.position = p.position + p.velocity;
+        p.velocity = p.velocity + e.gravity + e.wind;
+        ticks += 1;
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
