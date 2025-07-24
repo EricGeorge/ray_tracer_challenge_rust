4c59ba8bd969297d6a67ab956f0f05affe6c34e7
EricGeorge <eegee.dev@gmail.com>
2025-07-24 12:26:25 -0700

    Refactored Tuple into Point and Vector

--- examples/projectile.rs
+++ examples/projectile.rs
@@ -1,35 +1,54 @@
-use raytracer::tuple::*;
+use raytracer::point::*;
+use raytracer::vector::*;
 
 struct Projectile {
-    position: Tuple,
-    velocity: Tuple,
+    position: Point,
+    velocity: Vector,
+}
+
+impl Projectile {
+    fn new(position: Point, velocity: Vector) -> Self {
+        Self { position, velocity }
+    }
 }
 
 struct Environment {
-    gravity: Tuple,
-    wind: Tuple,
+    gravity: Vector,
+    wind: Vector,
+}
+
+impl Environment {
+    fn new(gravity: Vector, wind: Vector) -> Self {
+        Self { gravity, wind }
+    }
+}
+
+fn log(ticks: u16, position: Point) {
+    println!(
+        "Time: {:?}, x: {:?}, y: {:?}",
+        ticks, position.x, position.y
+    );
+}
+
+fn tick(p: &mut Projectile, e: &Environment) {
+    p.position = p.position + p.velocity;
+    p.velocity = p.velocity + e.gravity + e.wind;
 }
 
 fn main() {
-    let e = Environment {
-        gravity: vector(0.0, -0.1, 0.0),
-        wind: vector(-0.01, 0.0, 0.0),
-    };
+    let start = Point::new(0.0, 1.0, 0.0);
+    let velocity = Vector::new(1.0, 1.8, 0.0).normalize();
+    let mut p = Projectile::new(start, velocity);
 
-    let mut p = Projectile {
-        position: point(0.0, 1.0, 0.0),
-        velocity: (vector(1.0, 1.0, 0.0).normalize()) * 10.0,
-    };
+    let gravity = Vector::new(0.0, -0.1, 0.0);
+    let wind = Vector::new(-0.01, 0.0, 0.0);
+    let e = Environment::new(gravity, wind);
 
-    let mut ticks = 0;
+    let mut time = 0;
 
     while p.position.y > 0.0 {
-        println!(
-            "Time: {:?}, x: {:?}, y: {:?}",
-            ticks, p.position.x, p.position.y
-        );
-        p.position = p.position + p.velocity;
-        p.velocity = p.velocity + e.gravity + e.wind;
-        ticks += 1;
+        log(time, p.position);
+        tick(&mut p, &e);
+        time += 1;
     }
 }
