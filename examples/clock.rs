cb225453e12db1c20f40ee464a803b4d7962272e
Eric George <eegee.dev@gmail.com>
2025-07-30 21:48:05 -0700

    Clean up and comments...

--- examples/clock.rs
+++ examples/clock.rs
@@ -4,7 +4,6 @@ use raytracer::canvas::*;
 use raytracer::color::*;
 use raytracer::matrix::*;
 use raytracer::point::*;
-// use raytracer::vector::*;
 
 fn main() {
     let dim = 400.0;
@@ -13,26 +12,30 @@ fn main() {
 
     let angle_inc = FRAC_PI_6;
 
-    // radius is 3/8 of a side dimension
+    // radius of clock is 3/8 of a side dimension
     let r = 3.0 / 8.0 * dim;
+
+    // translation will be to the center
+    let tc = Matrix::translation(dim / 2.0, dim / 2.0, 0.0);
+
+    // translation to 12:00
+    let tt = Matrix::translation(r, 0.0, 0.0);
 
     for i in 0..12 {
         // initialize the point to the 12:00 position
         // remember that it is not centered yet...
         // and the canvas is on x and y axis.  We are rotating around z
-        let p = Point::new(r, 0.0, 0.0);
-
-        // translation will be to the center
-        let tt = Matrix::translation(dim / 2.0, dim / 2.0, 0.0);
+        let p = tt * Point::new(0.0, 0.0, 0.0);
 
         // rotation will be some multiple of angle_inc
         let tr = Matrix::rotation_z(angle_inc * i as f64);
 
-        // order doesn't seem to matter here...
-        let t = tr * tt;
+        //  and translate to the center along with rotating into position
+        // but remember that order matters...
+        let t = tc * tr;
 
         let h = t * p;
-        canvas.write_pixel(h.x.round() as usize, h.y.round() as usize, pixel_color);
+        canvas.write_pixel(h.x as usize, h.y as usize, pixel_color);
     }
 
     canvas.write_ppm("./clock.ppm");
